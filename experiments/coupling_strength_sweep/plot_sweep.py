25290d67e25c7b1bf1ad0c1aa81062059aa090ef
Gianluca Marcon <marcon.gluca@gmail.com>
2021-11-04 22:11:46 +0100

    updated sweep file

--- experiments/coupling_strength_sweep/plot_sweep.py
+++ experiments/coupling_strength_sweep/plot_sweep.py
@@ -18,7 +18,7 @@ def find_files(args):
     data_files = [os.path.join(args.directory, f) for f in filenames if f.endswith(".h5")]
     return data_files
 
-def get_data(args, index=None):
+def read_from_files(args, index=None):
     data_files = find_files(args)
     As = []
     Ap = []
@@ -42,6 +42,8 @@ def get_data(args, index=None):
 
     return As, Ap, z, np.array(Lk), Ps0
 
+
+
 def dBm(x):
     return 10 * np.log10(x * 1e3)
 
@@ -49,36 +51,45 @@ def dB(x):
     return 10 * np.log10(x)
 
 
-default_filename="/home/gianluca/sdm-propane/experiments/coupling_strength_sweep/random_polarizations/results/dz_1m_L_50km/"
 parser = argparse.ArgumentParser()
-parser.add_argument("directory", nargs='?', default=default_filename)
+parser.add_argument("directory", nargs='?')
 parser.add_argument("-s", "--save", action="store_true")
-args = parser.parse_args()
-As, Ap, z, Lk, Ps0 = get_data(args, index=-1)
+parser.add_argument("-S", "--save-data", action="store_true")
 
+args = parser.parse_args()
+As, Ap, z, Lk, Ps0 = read_from_files(args, index=-1)
 num_files = len(As)
 
-
-def compute_angle(a, b, axis=0):
-    norm_a = np.linalg.norm(a, axis=axis)
-    norm_b = np.linalg.norm(b, axis=axis)
-    ab = np.sum(a * b, axis=axis)
-    return ab
-
-
 std = []
-average_gain = []
-
-for x in range(num_files):
-    Ps = np.abs(As[x] ** 2)
-    Ps_pol = (Ps[:, ::2] + Ps[:, 1::2])
-    gain = dB(Ps_pol / Ps0[x])
-    average_gain.append(gain.mean(axis=0))
-    std.append(gain.std(axis=0))
+mean = []
 
 
-average_gain = np.stack(average_gain)
-std = np.stack(std)
+def compute_gain_statistics(args, index=None):
+    As, Ap, z, Lk, Ps0 = read_from_files(args, index=index)
+    num_files = len(As)
+    std = []
+    average_gain = []
+    for i in range(num_files):
+        Ps = np.abs(As[i]) ** 2
+        Ps_pol = (Ps[:, ::2] + Ps[:, 1::2])
+        gain = dB(Ps_pol / Ps0[i])
+        average_gain.append(gain.mean(axis=0))
+        std.append(gain.std(axis=0))
+
+
+    average_gain = np.stack(average_gain)
+    std = np.stack(std)
+    return Lk, average_gain, std
+
+
+
+# def compute_angle(a, b, axis=0):
+#     norm_a = np.linalg.norm(a, axis=axis)
+#     norm_b = np.linalg.norm(b, axis=axis)
+#     ab = np.sum(a * b, axis=axis)
+#     return ab
+
+
 
 # S_s = polarization.hyperjones_to_hyperstokes(As, axis=-1)
 # S_p = polarization.hyperjones_to_hyperstokes(Ap, axis=-1)
@@ -110,19 +121,33 @@ std = np.stack(std)
 # idx = lengths * 1e3 / dz
 
 
-nmodes = average_gain.shape[-1]
+# check if passed parameter is a directory
+if os.path.isdir(args.directory):
+    Lk, mean, std = compute_gain_statistics(args, index=-1)
+
+    # save arrays in numpy file
+    if args.save_data:
+        data_file = os.path.join(args.directory, "data.npz")
+        np.savez(data_file, Lk=Lk, average_gain=mean, std=std)
+else:
+    # read from npz data file
+    data = np.load(args.directory)
+    Lk = data['Lk']
+    mean = data['mean']
+    std = data['std']
+
+
+nmodes = mean.shape[-1]
 mode_labels = ["LP01", "LP11a", "LP11b"]
 markers = ["o", 's', 'x', '^']
 colors= [ f"C{m}" for m in range(nmodes) ] 
 mode_handles = [lines.Line2D([], [], color=colors[x], marker=markers[x], label=mode_labels[x]) for x in range(nmodes)]
 
-
 fig, axs = plt.subplots(nrows=2, sharex=True)
-
 for m in range(nmodes):
     color = colors[m]
     marker = markers[m]
-    axs[0].semilogx(Lk, average_gain[:, m].squeeze(), color=color, marker=marker, fillstyle='none')
+    axs[0].semilogx(Lk, mean[:, m].squeeze(), color=color, marker=marker, fillstyle='none')
     axs[0].set_ylabel(r"$\langle G \rangle$ [dB]")
 
     axs[1].semilogx(Lk, std[:, m].squeeze(), color=color, marker=marker, fillstyle='none')
@@ -131,10 +156,11 @@ for m in range(nmodes):
 axs[0].legend(handles=mode_handles)
 plt.tight_layout()
 
-output_file = os.path.join(args.directory, "plot.png")
 
 
 if args.save:
+    output_file = os.path.join(args.directory, "plot.png")
     plt.savefig(output_file, dpi=500)
 else:
     plt.show()
+
